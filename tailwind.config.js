6b9875b8b1b25b9434450c0f45af33a4515043f5
Edward Vidal <ehvidalp@gmail.com>
2025-07-04 20:41:39 -0600

    feat: add Star Wars font and enhance welcome component styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,4 +3,11 @@ module.exports = {
   content: [
     "./src/**/*.{html,ts,js,jsx,tsx}",
   ],
+  theme: {
+    extend: {
+      fontFamily: {
+        starwars: ['starwars', 'monospace'],
+      },
+    },
+  },
 };
